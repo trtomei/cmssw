5a010b0305a4b2d0a35bc625f6eb94c9e0b2f05a
Andrea Bocci <andrea.bocci@cern.ch>
2021-03-09 17:54:41 +0100

    Update the configuration for CMSSW 11.1.8

--- HLTrigger/Phase2/python/HLT_75e33/modules/ticlSeedingTrk_cfi.py
+++ HLTrigger/Phase2/python/HLT_75e33/modules/ticlSeedingTrk_cfi.py
@@ -1,10 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 ticlSeedingTrk = cms.EDProducer("TICLSeedingRegionProducer",
-    algoId = cms.int32(1),
-    algo_verbosity = cms.int32(0),
-    cutTk = cms.string('1.48 < abs(eta) < 3.0 && pt > 1. && quality("highPurity") && hitPattern().numberOfLostHits("MISSING_OUTER_HITS") < 5'),
     mightGet = cms.optional.untracked.vstring,
-    propagator = cms.string('PropagatorWithMaterial'),
-    tracks = cms.InputTag("generalTracks")
+    seedingPSet = cms.PSet(
+        algo_verbosity = cms.int32(0),
+        type = cms.string('SeedingRegionByTracks')
+    )
 )
