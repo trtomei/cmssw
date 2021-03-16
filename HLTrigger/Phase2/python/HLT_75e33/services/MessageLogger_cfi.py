da782c4d280144b9cd6b1bdbc8f4c44f591f9a1f
Andrea Bocci <andrea.bocci@cern.ch>
2021-03-16 12:04:22 +0100

    Update MessageLogger configuration

--- HLTrigger/Phase2/python/HLT_75e33/services/MessageLogger_cfi.py
+++ HLTrigger/Phase2/python/HLT_75e33/services/MessageLogger_cfi.py
@@ -1,6 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 MessageLogger = cms.Service("MessageLogger",
+    default = cms.untracked.PSet(
+    ),
+    fwkJobReports = cms.untracked.vstring('FrameworkJobReport'),
     FrameworkJobReport = cms.untracked.PSet(
         FwkJob = cms.untracked.PSet(
             limit = cms.untracked.int32(10000000),
@@ -17,14 +20,21 @@ MessageLogger = cms.Service("MessageLogger",
         'FwkSummary', 
         'Root_NoDictionary'
     ),
+    destinations = cms.untracked.vstring('cerr'),
     cerr = cms.untracked.PSet(
+        default = cms.untracked.PSet(
+            limit = cms.untracked.int32(10000000)
+        ),
+        threshold = cms.untracked.string('INFO'),
+        INFO = cms.untracked.PSet(
+            limit = cms.untracked.int32(0)
+        ),
         FwkJob = cms.untracked.PSet(
             limit = cms.untracked.int32(0),
             optionalPSet = cms.untracked.bool(True)
         ),
         FwkReport = cms.untracked.PSet(
             limit = cms.untracked.int32(10000000),
-            optionalPSet = cms.untracked.bool(True),
             reportEvery = cms.untracked.int32(1)
         ),
         FwkSummary = cms.untracked.PSet(
@@ -32,60 +42,22 @@ MessageLogger = cms.Service("MessageLogger",
             optionalPSet = cms.untracked.bool(True),
             reportEvery = cms.untracked.int32(1)
         ),
-        INFO = cms.untracked.PSet(
-            limit = cms.untracked.int32(0)
-        ),
         Root_NoDictionary = cms.untracked.PSet(
             limit = cms.untracked.int32(0),
             optionalPSet = cms.untracked.bool(True)
-        ),
-        default = cms.untracked.PSet(
-            limit = cms.untracked.int32(10000000)
         ),
         noTimeStamps = cms.untracked.bool(False),
-        optionalPSet = cms.untracked.bool(True),
-        threshold = cms.untracked.string('INFO')
-    ),
-    cerr_stats = cms.untracked.PSet(
-        optionalPSet = cms.untracked.bool(True),
-        output = cms.untracked.string('cerr'),
-        threshold = cms.untracked.string('WARNING')
-    ),
-    cout = cms.untracked.PSet(
-        placeholder = cms.untracked.bool(True)
-    ),
-    debugModules = cms.untracked.vstring(),
-    debugs = cms.untracked.PSet(
-        placeholder = cms.untracked.bool(True)
-    ),
-    default = cms.untracked.PSet(
-
-    ),
-    destinations = cms.untracked.vstring(
-        'warnings', 
-        'errors', 
-        'infos', 
-        'debugs', 
-        'cout', 
-        'cerr'
-    ),
-    errors = cms.untracked.PSet(
-        placeholder = cms.untracked.bool(True)
-    ),
-    fwkJobReports = cms.untracked.vstring('FrameworkJobReport'),
-    infos = cms.untracked.PSet(
-        Root_NoDictionary = cms.untracked.PSet(
-            limit = cms.untracked.int32(0),
-            optionalPSet = cms.untracked.bool(True)
-        ),
-        optionalPSet = cms.untracked.bool(True),
-        placeholder = cms.untracked.bool(True)
+        optionalPSet = cms.untracked.bool(True)
     ),
     statistics = cms.untracked.vstring('cerr_stats'),
+    cerr_stats = cms.untracked.PSet(
+        output = cms.untracked.string('cerr'),
+        threshold = cms.untracked.string('WARNING'),
+        optionalPSet = cms.untracked.bool(True)
+    ),
+    debugModules = cms.untracked.vstring(),
     suppressDebug = cms.untracked.vstring(),
     suppressInfo = cms.untracked.vstring(),
-    suppressWarning = cms.untracked.vstring('particleFlowDisplacedVertexCandidate'),
-    warnings = cms.untracked.PSet(
-        placeholder = cms.untracked.bool(True)
-    )
+    suppressWarning = cms.untracked.vstring(),
+    suppressError = cms.untracked.vstring()
 )
