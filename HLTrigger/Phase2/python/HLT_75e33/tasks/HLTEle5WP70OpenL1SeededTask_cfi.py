dcb56b144f76339f2abe2a68704efbcffe4fa11b
Andrea Bocci <andrea.bocci@cern.ch>
2021-05-06 22:03:09 +0200

    Remove hltEgammaEleGsfTrackIsoV72L1Seeded (not used)

--- HLTrigger/Phase2/python/HLT_75e33/tasks/HLTEle5WP70OpenL1SeededTask_cfi.py
+++ HLTrigger/Phase2/python/HLT_75e33/tasks/HLTEle5WP70OpenL1SeededTask_cfi.py
@@ -5,7 +5,6 @@ from ..modules.hltEgammaClusterShapeL1Seeded_cfi import *
 from ..modules.hltEgammaEcalPFClusterIsoL1Seeded_cfi import *
 from ..modules.hltEgammaEleGsfTrackIsoL1Seeded_cfi import *
 from ..modules.hltEgammaEleGsfTrackIsoV6L1Seeded_cfi import *
-from ..modules.hltEgammaEleGsfTrackIsoV72L1Seeded_cfi import *
 from ..modules.hltEgammaEleL1TrkIsoL1Seeded_cfi import *
 from ..modules.hltEgammaHcalPFClusterIsoL1Seeded_cfi import *
 from ..modules.hltEgammaHGCALIDVarsL1Seeded_cfi import *
@@ -18,7 +17,6 @@ HLTEle5WP70OpenL1SeededTask = cms.Task(
     hltEgammaEcalPFClusterIsoL1Seeded,
     hltEgammaEleGsfTrackIsoL1Seeded,
     hltEgammaEleGsfTrackIsoV6L1Seeded,
-    hltEgammaEleGsfTrackIsoV72L1Seeded,
     hltEgammaEleL1TrkIsoL1Seeded,
     hltEgammaHGCALIDVarsL1Seeded,
     hltEgammaHGCalLayerClusterIsoL1Seeded,
